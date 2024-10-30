64c7368956974b7e591524998c90a766571f76e1
Kpiiale <yuliana.capito@udla.edu.ec>
2024-10-30 15:58:34 -0500

    Migraciones

---
+++ CapitoYuliana-ExamenProgreso1/Migrations/20241030205738_MigracionInicial.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapitoYuliana_ExamenProgreso1.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Celular",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Anio = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Celular", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Capito",
+                columns: table => new
+                {
+                    Cedula = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Altura = table.Column<float>(type: "real", nullable: false),
+                    MayorDeEdad = table.Column<bool>(type: "bit", nullable: false),
+                    celularId = table.Column<int>(type: "int", nullable: false),
+                    idCelular = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Capito", x => x.Cedula);
+                    table.ForeignKey(
+                        name: "FK_Capito_Celular_celularId",
+                        column: x => x.celularId,
+                        principalTable: "Celular",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Capito_celularId",
+                table: "Capito",
+                column: "celularId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Capito");
+
+            migrationBuilder.DropTable(
+                name: "Celular");
+        }
+    }
+}
